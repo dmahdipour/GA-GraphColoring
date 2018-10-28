c3cf8e18d4cc3846354e0ceba26cb6219f4a232d
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 22:18:36 +0330

    Modify main.m to initial Population and flags

--- main.m
+++ main.m
@@ -11,4 +11,12 @@ resData=zeros(maxRun,L);
 for runTime=1:maxRun
     % Inputs 
     [L,myGraph,colorN,Gmax,Pc,Pm,ElitismOF,ElitismData,popNum] = inputsFunc();
+    
+    % Generate Pop
+    PopT=ceil(colorN*rand(popNum,L));
+    PopFirst=PopT;
+
+    flgEnd=0;
+    counterG=1;
+    
 end
